a5ed439712dbf1121c5820c96a4a9ffdd7a2e3fd
Tam Cong Tam <tamthanhsk25@gmail.com>
2023-06-21 11:06:39 +0700

    add header

--- tailwindcss-coffee/tailwind.config.js
+++ tailwindcss-coffee/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'light-coffee': '#c89f94'
+      }
+    },
   },
   plugins: [],
 }
